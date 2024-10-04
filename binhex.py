616d2e911f4ff170bc1627ff490c3f269eb795a7
Grisshink <griss@grisshink.com>
2024-10-04 16:21:21 +0300

    Python - Simplify decryption code

--- binhex.py
+++ binhex.py
@@ -1,8 +1,8 @@
 from itertools import cycle, batched
+
 def decrypt_hex(msg, key):
-    def strhex(inp): return (int(''.join(block), 16) for block in batched(inp, 4))
-    dec = (left ^ right for left, right in zip(strhex(msg), cycle(strhex(key))))
-    return ''.join(chr(block + 31) for block in dec)[:-1]
+    dec = (m ^ k for m, k in zip(bytes.fromhex(msg), cycle(bytes.fromhex(key))))
+    return ''.join(chr((hi << 8 | lo) + 31) for hi, lo in batched(dec, 2))[:-1]
 
 def encrypt_hex(msg, key):
     enc = ((ord(char) - 31) ^ int(''.join(block), 16) for char, block in zip(msg + '0', cycle(batched(key, 4))))
